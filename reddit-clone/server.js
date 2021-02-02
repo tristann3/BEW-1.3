4468f207b8a81aa55182277ea559cbe66f02a64e
Tristan Thompson <tristan15thompson@gmail.com>
2021-02-01 20:06:27 -0800

    [ADD] added bootstrap navbar

--- reddit-clone/server.js
+++ reddit-clone/server.js
@@ -1,10 +1,15 @@
+const { response } = require("express");
 const express = require("express");
 const exphbs = require("express-handlebars");
 
 const app = express();
 
-app.engine = ("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.get("", (req, res) => {
+  res.render("layouts/main");
+});
 
 app.listen(3000, () => {
   console.log("Listening on Port 3000!");
