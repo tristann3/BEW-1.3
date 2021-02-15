0914dc11294c4cab743f45a6ba6e8b385251f890
Tristan Thompson <tristan15thompson@gmail.com>
2021-02-14 22:25:08 -0800

    [ADD] Users can save and submit posts

---
+++ reddit-clone/src/routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const postsRoutes = require("./posts.js");
+
+const router = express.Router();
+
+router.use("/posts", postsRoutes);
+
+module.exports = router;
--- reddit-clone/src/server.js
+++ reddit-clone/src/server.js
@@ -1,11 +1,24 @@
 require("dotenv/config");
 const express = require("express");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+// Set db
+require("./data/reddit-db");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(expressValidator());
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// All middlewares above this comment
+const router = require("./routes/index.js");
+app.use(router);
 
 app.get("/", (req, res) => {
   res.render("home");
