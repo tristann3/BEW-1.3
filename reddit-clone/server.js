9ee21f91cf1373d40fa3512a2f50e905c94efd82
Tristan Thompson <tristan15thompson@gmail.com>
2021-02-14 16:14:22 -0800

    [ADD] node reddit clone project init"

---
+++ reddit-clone/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!");
+});
