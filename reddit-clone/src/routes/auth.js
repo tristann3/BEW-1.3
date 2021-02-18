8d5a8609d2ce34c9c8481fee5fb3a2ebc3bd7978
Tristan Thompson <tristan15thompson@gmail.com>
2021-02-17 20:54:54 -0800

    [ADD] Users can sign in

--- reddit-clone/src/routes/auth.js
+++ reddit-clone/src/routes/auth.js
@@ -9,6 +9,11 @@ router.get("/sign-up", (req, res) => {
   res.render("sign-up");
 });
 
+// LOGIN FORM
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
 // SIGN UP POST
 router.post("/sign-up", (req, res) => {
   // Create User and JWT
@@ -26,6 +31,43 @@ router.post("/sign-up", (req, res) => {
     .catch((err) => {
       console.log(err.message);
       return res.status(400).send({ err: err });
+    });
+});
+
+// LOGIN
+router.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // Find this user name
+  User.findOne({ username }, "username password")
+    .then((user) => {
+      if (!user) {
+        // User not found
+        return res.status(401).send({ message: "Wrong Username or Password" });
+      }
+      // Check the password
+      user.comparePassword(password, (err, isMatch) => {
+        if (!isMatch) {
+          // Password does not match
+          return res
+            .status(401)
+            .send({ message: "Wrong Username or password" });
+        }
+        // Create a token
+        const token = jwt.sign(
+          { _id: user._id, username: user.username },
+          process.env.SECRET,
+          {
+            expiresIn: "60 days",
+          }
+        );
+        // Set a cookie and redirect to root
+        res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+        res.redirect("/");
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });
 
